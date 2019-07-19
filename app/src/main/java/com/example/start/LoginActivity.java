da69b68e94b38836fa019011b04e3bf135fb5a91
rahulkota <rahulkota1309@gmail.com>
2019-07-19 18:26:24 +0530

    first project till here no errors and this is until the login page for the students

--- app/src/main/java/com/example/start/LoginActivity.java
+++ app/src/main/java/com/example/start/LoginActivity.java
@@ -15,5 +15,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         edtid=findViewById(R.id.edtid);
+        edtname=findViewById(R.id.edtname);
     }
 }
