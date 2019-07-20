92255ab7025d926f84d84057f8e5164f21c57717
rahulkota <rahulkota1309@gmail.com>
2019-07-20 08:51:45 +0530

    successfully changed my  actionbar theme in results activity to no action bar and added my tabbed layout

--- app/src/main/java/com/example/start/Results.java
+++ app/src/main/java/com/example/start/Results.java
@@ -1,14 +1,35 @@
 package com.example.start;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class Results extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        setTitle("Results");
+
+        toolbar=findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+        viewPager=findViewById(R.id.viewpager);
+        tabAdapter=new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+    tabLayout=findViewById(R.id.tabLayout);
+    tabLayout.setupWithViewPager(viewPager,false);
+
     }
 }
---
+++ app/src/main/java/com/example/start/TabAdapter.java
@@ -0,0 +1,52 @@
+package com.example.start;
+
+import android.widget.Switch;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabAdapter extends FragmentPagerAdapter {
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int tabposition) {
+
+        switch(tabposition){
+            case 0:
+                NewsTab newsTab=new NewsTab();
+                return newsTab;
+            case 1:
+                ClubsTab clubsTab=new ClubsTab();
+                return clubsTab;
+            case 2:
+                return new DescriptionTab();
+                default:
+                    return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int tabposition) {
+       switch(tabposition){
+
+           case 0:
+               return"News";
+           case 1:
+               return "Clubs";
+           case 2:
+               return "Description";
+               default:
+                   return null;
+       }
+    }
+}
